45a387f3b38bfdafbdac963e556e2eccf086eae8
Mikhail Trifonov <mstrifonov@yandex.ru>
2025-06-09 15:03:58 +0300

    initial

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Applications/work/devplatform.pro/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Applications/work/devplatform.pro/node_modules/infima/dist/css/default/default.css"),
+  require("/Applications/work/devplatform.pro/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Applications/work/devplatform.pro/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Applications/work/devplatform.pro/src/css/custom.css"),
+];
---
+++ build/assets/js/aba21aa0.e64de273.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkidp=self.webpackChunkidp||[]).push([[742],{7093:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
